771a3f10dbf93037933c229948e7743fca52ae0b
kapsner <lorenz.kapsner@gmail.com>
2022-10-10 13:46:13 +0200

    ci: solve warning for missing global variables

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,10 @@
 #' @importFrom R6 R6Class
 NULL
 
+# https://community.rstudio.com/t/how-to-solve-no-visible-binding-for-global-
+# variable-note/28887
+utils::globalVariables(c("seed", "method_helper", "x", "y"))
+
 mlexperiments_default_options <- list(
   # mlexperiments.learner = c(
   #   LearnerKnn$classname, # = "LearnerSurvGlmnetCox"
