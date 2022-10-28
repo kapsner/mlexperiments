22b6cdcd01421a196014b048d8f2dd59f1e9c6f6
kapsner <lorenz.kapsner@web.de>
2022-10-28 16:36:25 +0200

    feat: metric_types_helper again from kdry

--- R/utils_metrics.R
+++ R/utils_metrics.R
@@ -69,3 +69,53 @@ metric <- function(name) {
   )
   return(eval(parse(text = fun)))
 }
+
+#' @export
+metric_types_helper <- function(FUN, y, perf_args) {
+  stopifnot(is.function(FUN), is.list(perf_args),
+            all(c("ground_truth", "predictions") %in% names(perf_args)))
+  # note that this is very specific to the mlr3measures package
+  tryCatch(
+    expr = {
+      return(do.call(FUN, perf_args))
+    }, error = function(e) {
+      if (!is.factor(y)) {
+        y <- factor(y)
+      }
+      lvls <- levels(y)
+      error <- TRUE
+      if (grepl(
+        pattern = "Assertion on 'truth' failed: Must be of type 'factor'",
+        x = e
+      )) {
+        # convert to factor
+        perf_args$ground_truth <- factor(
+          x = perf_args$ground_truth,
+          levels = lvls
+        )
+        error <- FALSE
+      } else if (grepl(
+        pattern = "Assertion on 'response' failed: Must be of type 'factor'",
+        x = e
+      )) {
+        perf_args$predictions <- factor(
+          x = perf_args$predictions,
+          levels = lvls
+        )
+        error <- FALSE
+      }
+
+      if (isFALSE(error)) {
+        # recursive execution
+        args <- list(
+          FUN = FUN,
+          y = y,
+          perf_args = perf_args
+        )
+        return(do.call(mlh_fix_performance_types, args))
+      } else {
+        stop(e)
+      }
+    }
+  )
+}
