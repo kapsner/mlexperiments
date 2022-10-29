2ba876e5b7d807fd59b0bdc1b3b152bd9c4052e0
kapsner <lorenz.kapsner@web.de>
2022-10-29 18:12:09 +0200

    ci: try to fix ci

--- tic.R
+++ tic.R
@@ -2,6 +2,6 @@
 do_package_checks()
 
 get_stage("install") %>%
-  add_step(step_install_github("kapsner/kdry")) %>%
+  add_code_step(remotes::install_github("kapsner/kdry", dependencies = TRUE)) %>%
   add_code_step(devtools::install(".", upgrade = "always"))
 
