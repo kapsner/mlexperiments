59b95e0191e8b7c1d7c3b8eab55f2a577ab039bf
kapsner <lorenz.kapsner@web.de>
2022-10-29 17:44:34 +0200

    ci: installing dependencies for kdry

--- tic.R
+++ tic.R
@@ -2,5 +2,6 @@
 do_package_checks()
 
 get_stage("install") %>%
+  add_step(step_install_github("kapsner/kdry", dependencies = TRUE)) %>%
   add_code_step(devtools::install(".", upgrade = "always"))
 
