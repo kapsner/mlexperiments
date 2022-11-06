97f3dbee1a0658345d0456530b0272187f663ac4
kapsner <lorenz.kapsner@web.de>
2022-11-06 07:25:22 +0100

    ci: removed vignette building from ci

--- tic.R
+++ tic.R
@@ -1,5 +1,8 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
-do_package_checks()
+do_package_checks(
+  args = "--no-vignettes",
+  build_args = "--no-build-vignettes"
+)
 
 get_stage("install") %>%
   add_step(step_install_cran("doParallel")) %>%
