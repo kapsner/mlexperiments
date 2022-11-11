30ba8758e7059e78654f9e6db8bd7ec1c1727df4
kapsner <lorenz.kapsner@gmail.com>
2022-11-11 07:58:09 +0100

    ci: updated ci

--- tic.R
+++ tic.R
@@ -1,10 +1,10 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
 do_package_checks(
   args = "--no-vignettes",
-  build_args = "--no-build-vignettes"
+  build_args = "--no-build-vignettes",
+  error_on = "error"
 )
 
 get_stage("install") %>%
   add_step(step_install_cran("doParallel")) %>%
   add_code_step(devtools::install(".", upgrade = "always"))
-
