ad992cb4c917198fbf85604c9c20b613716176e9
kapsner <lorenz.kapsner@gmail.com>
2026-01-16 05:42:13 +0100

    chore: provide ncores to Bayesian scoring fun rpart

--- R/learner_rpart.R
+++ R/learner_rpart.R
@@ -121,15 +121,13 @@ rpart_bsF <- function(...) { # nolint
     append_list = method_helper$execute_params["cat_vars"]
   )
 
-  # call to rpart_optimization here with ncores = 1, since the Bayesian search
-  # is parallelized already / "FUN is fitted n times in m threads"
   set.seed(seed)#, kind = "L'Ecuyer-CMRG")
   bayes_opt_rpart <- rpart_optimization(
     x = x,
     y = y,
     params = params,
     fold_list = method_helper$fold_list,
-    ncores = 1L, # important, as bayesian search is already parallelized
+    ncores = ncores,
     seed = seed
   )
 
